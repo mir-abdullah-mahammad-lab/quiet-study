f7b2e452d19819e164c9d318ce3e9bfb59d70fce
mir-abdullah-mahammad-lab <mirabdullahmahammad@gmail.com>
2026-06-08 08:22:01 +0600

    firstTestigCommit

---
+++ src/app/allrooms/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const allLibraryRooms = () => {
+    return (
+        <div>
+            <h1>All Library Rooms</h1>
+        </div>
+    );
+};
+
+export default allLibraryRooms;
